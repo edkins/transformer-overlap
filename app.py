76c950473cd6b962dc6b0accc9964ef1e974c346
Giles Edkins <edkins@gmail.com>
2023-07-03 20:23:41 -0400

    Retrieve and visualize attention patterns

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, redirect, request
+from flask import Flask, redirect, request, Response
+from jsonschema import validate
 import torch
 import transformer_lens
 
@@ -25,12 +26,20 @@ def _get_model(model_name):
 def index():
     return redirect('static/index.html')
 
+schema_tokenize = {
+    'type': 'object',
+    'properties': {
+        'prompt_text': {'type': 'string'},
+        'model_name': {'type': 'string'},
+    },
+    'required': ['prompt_text', 'model_name'],
+}
+
 @app.post('/api/tokenize')
-def put_document():
+def tokenize():
     j = request.json
+    validate(instance=j, schema=schema_tokenize)
     text = j['prompt_text']
-    if not isinstance(text, str):
-        raise ValueError(f'text must be a string, not {type(text)}')
     model_name = j['model_name']
     model = _get_model(model_name)
     tokens = model.to_tokens(text)[0]
@@ -46,3 +55,68 @@ def put_document():
             'tokens': [t.item() for t in tokens],
         }
     }
+
+schema_activations = {
+    'type': 'object',
+    'properties': {
+        'prompt_text': {'type': 'string'},
+        'model_name': {'type': 'string'},
+        'byte_length': {'type': 'integer', 'minimum': 0},
+        'sections': {
+            'type': 'array',
+            'items': {
+                'type': 'object',
+                'properties': {
+                    'name': {'type': 'string'},
+                    'byte_offset': {'type': 'integer', 'minimum': 0},
+                    'byte_length': {'type': 'integer', 'minimum': 0},
+                    'dims': {'type': 'array', 'items': {'type': 'integer', 'minimum': 0}},
+                    'dtype': {'enum': ['float32']},
+                },
+                'required': ['name', 'byte_offset', 'byte_length', 'dims', 'dtype'],
+            },
+        },
+    },
+    'required': ['prompt_text', 'model_name', 'byte_length', 'sections'],
+}
+
+def _extract_from_cache(model, cache, n_tokens, name: str, dims: list[int], dtype: str) -> bytearray:
+    n_layers = model.cfg.n_layers
+    n_heads = model.cfg.n_heads
+    d_model = model.cfg.d_model
+    if name == 'attention' and dtype == 'float32':
+        t = torch.zeros((n_layers, n_heads, n_tokens, n_tokens), dtype=torch.float32)
+        for layer in range(n_layers):
+            t[layer,:,:,:] = cache[f'blocks.{layer}.attn.hook_pattern']
+    else:
+        raise ValueError(f'Unsupported name/dtype combination: {name}/{dtype}')
+
+    if tuple(t.shape) != tuple(dims):
+        raise ValueError(f'Expected dims {dims}, got {t.shape}')
+
+    return t.numpy().tobytes()
+
+@app.post('/api/activations')
+def get_activations():
+    j = request.json
+    validate(instance=j, schema=schema_activations)
+    if j['byte_length'] > 1_000_000_000:
+        raise ValueError("byte_length is too huge")
+    text = j['prompt_text']
+    model = _get_model(j['model_name'])
+    result = bytearray(j['byte_length'])
+    n_tokens = model.to_tokens(text).shape[1]
+    _, cache = model.run_with_cache(text)
+    for section in j['sections']:
+        name = section['name']
+        byte_offset = section['byte_offset']
+        byte_length = section['byte_length']
+        dims = section['dims']
+        dtype = section['dtype']
+        if byte_offset + byte_length > j['byte_length']:
+            raise ValueError("byte_length is too small. Section won't fit.")
+        chunk = _extract_from_cache(model, cache, n_tokens, name, dims, dtype)
+        if len(chunk) != byte_length:
+            raise ValueError(f'Expected byte_length {byte_length}, got {len(chunk)}')
+        result[byte_offset:byte_offset+byte_length] = chunk
+    return Response(bytes(result), mimetype='application/octet-stream')
--- static/transformer_visualization.js
+++ static/transformer_visualization.js
@@ -8,6 +8,10 @@ function _draw_transformer(model) {
     const width = svg.clientWidth;
     const height = svg.clientHeight;
     const margin = 5;
+    const box_space = (height - 2 * margin) / model.n_layers;
+    const mlp_height = box_space * 0.3 - 2 * margin;
+    const mlp_width = mlp_height * 3;
+    const box_size = box_space * 0.5;
 
     const residual_stream = document.createElementNS('http://www.w3.org/2000/svg', 'line');
     residual_stream.setAttribute('x1', margin);
@@ -16,10 +20,6 @@ function _draw_transformer(model) {
     residual_stream.setAttribute('y2', margin);
     residual_stream.setAttribute('stroke', 'black');
 
-    const box_space = (height - 2 * margin) / model.n_layers;
-    const mlp_height = box_space * 0.3 - 2 * margin;
-    const mlp_width = mlp_height * 3;
-    const box_size = box_space * 0.5;
     for (let layer = 0; layer < model.n_layers; layer++) {
         const resid_broadcast = document.createElementNS('http://www.w3.org/2000/svg', 'line');
         resid_broadcast.setAttribute('x1', margin);
@@ -44,7 +44,7 @@ function _draw_transformer(model) {
             attn_box.setAttribute('width', box_size);
             attn_box.setAttribute('height', box_size);
             attn_box.setAttribute('stroke', 'black');
-            attn_box.setAttribute('fill', 'none');
+            attn_box.setAttribute('fill', '#888');
             svg.appendChild(attn_box);
 
             const resid_out = document.createElementNS('http://www.w3.org/2000/svg', 'line');
@@ -93,6 +93,37 @@ function _draw_transformer(model) {
     svg.appendChild(residual_stream);
 }
 
+function _draw_attention(n_layers, n_heads, n_tokens, dataview) {
+    const svg = document.getElementById('svg');
+    const height = svg.clientHeight;
+    const margin = 5;
+    const box_space = (height - 2 * margin) / n_layers;
+    const mlp_height = box_space * 0.3 - 2 * margin;
+    const box_size = box_space * 0.5;
+
+    for (let layer = 0; layer < n_layers; layer++) {
+        for (let head = 0; head < n_heads; head++) {
+            for (let token0 = 0; token0 < n_tokens; token0++) {
+                for (let token1 = 0; token1 <= token0; token1++) {
+                    const ix = ((((layer * n_heads) + head) * n_tokens) + token0) * n_tokens + token1;
+                    const value = dataview.getFloat32(ix * 4, true);
+                    const r = Math.min(Math.floor(1000 * value), 255);
+                    const g = Math.min(Math.floor(300 * value), 255);
+                    const b = Math.min(Math.floor(100 * value), 255);
+                    const color = `rgb(${r}, ${g}, ${b})`;
+                    const rect = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
+                    rect.setAttribute('x', head * box_space + 2 * margin + token1 * (box_size / n_tokens));
+                    rect.setAttribute('y', layer * box_space + 4 * margin + mlp_height + (n_tokens - 1 - token0) * (box_size / n_tokens));
+                    rect.setAttribute('width', box_size / n_tokens);
+                    rect.setAttribute('height', box_size / n_tokens);
+                    rect.setAttribute('fill', color);
+                    svg.appendChild(rect);
+                }
+            }
+        }
+    }
+}
+
 async function transformer_visualization() {
     const prompt_text = document.getElementById('prompt').value;
     const model_name = document.getElementById('model').value;
@@ -107,10 +138,42 @@ async function transformer_visualization() {
         })
     });
     if (!response.ok) {
-        _handle_error("Error from server");
+        _handle_error("Error from server when tokenizing prompt.");
         return;
     }
-    const data = await response.json();
-    console.log(data);
-    _draw_transformer(data.model);
+    const prompt_data = await response.json();
+
+    const n_tokens = prompt_data.prompt.tokens.length;
+
+    let byte_offset = 0;
+    const sections = [];
+    sections.push({
+        name: 'attention',
+        byte_offset,
+        byte_length: prompt_data.model.n_layers * prompt_data.model.n_heads * n_tokens * n_tokens * 4,
+        dims: [prompt_data.model.n_layers, prompt_data.model.n_heads, n_tokens, n_tokens],
+        dtype: 'float32',
+    });
+    byte_offset += sections[0].byte_length;
+
+    const response2 = await fetch('/api/activations', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            prompt_text,
+            model_name,
+            byte_length: byte_offset,
+            sections,
+        })
+    });
+    if (!response2.ok) {
+        _handle_error("Error from server with activations");
+        return;
+    }
+    const result_byte_array = await response2.arrayBuffer();
+    const activation_dataview = new DataView(result_byte_array, sections[0].byte_offset, sections[0].byte_length);
+    _draw_transformer(prompt_data.model);
+    _draw_attention(prompt_data.model.n_layers, prompt_data.model.n_heads, n_tokens, activation_dataview);
 }
