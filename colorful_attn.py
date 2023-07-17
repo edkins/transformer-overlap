fd7af8b2717f6ec2eafa2aed7fb41f77d8687727
Giles Edkins <edkins@gmail.com>
2023-07-16 23:09:39 -0400

    Do normalization after, and allow selecting a specific head to view

--- colorful_attn.py
+++ colorful_attn.py
@@ -19,6 +19,17 @@ d_model = model.cfg.d_model
 d_head = model.cfg.d_head
 print(f'n_layers = {n_layers}, n_heads = {n_heads}, d_model = {d_model}')
 
+if len(sys.argv) > 2:
+    min_layer = int(sys.argv[2])
+    min_head = int(sys.argv[3])
+    max_layer = min_layer + 1
+    max_head = min_head + 1
+else:
+    min_layer = 0
+    min_head = 0
+    max_layer = n_layers
+    max_head = n_heads
+
 toks = model.to_tokens(prompt)[0]
 n_toks = len(toks)
 print(f'prompt = {prompt}')
@@ -29,33 +40,38 @@ for i,t in enumerate(toks):
 alg = 2
 
 predictions, cache = model.run_with_cache(prompt)
-fig, ax = plt.subplots(n_layers, n_heads)
-for layer in range(n_layers):
-    for head in range(n_heads):
+fig, ax = plt.subplots(max_layer - min_layer, max_head - min_head, squeeze=False)
+for layer in range(min_layer, max_layer):
+    for head in range(min_head, max_head):
         k = cache[f'blocks.{layer}.attn.hook_k'][0,:,head,:]
         q = cache[f'blocks.{layer}.attn.hook_q'][0,:,head,:]
         a = cache[f'blocks.{layer}.attn.hook_pattern'][0][head,:,:]
 
         if alg == 1:
-            kq = torch.nn.functional.normalize(torch.concatenate((
+            kq = torch.concatenate((
                 k.reshape(1, n_toks, d_head).expand(n_toks, n_toks, d_head),
                 q.reshape(n_toks, 1, d_head).expand(n_toks, n_toks, d_head),
-            ), dim=2), dim=2)
+            ), dim=2)
             dkq = 2 * d_head
         elif alg == 2:
-            kq = torch.nn.functional.normalize(
-                k.reshape(1, n_toks, d_head) * q.reshape(n_toks, 1, d_head),
-                dim=2)
+            kq = k.reshape(1, n_toks, d_head) * q.reshape(n_toks, 1, d_head)
             dkq = d_head
         else:
             raise ValueError(f'Unknown alg {alg}')
+
+        # erase lower (upper?) triangle of kq
+        mask = torch.tril(torch.ones(n_toks, n_toks), diagonal=1).to(device)
+        kq = kq * mask.reshape(n_toks, n_toks, 1)
+
         n_toks1 = n_toks - 1
-        kqa = (kq * a.reshape(n_toks, n_toks, 1))[1:,1:,:]  # remove first token
+        kq = kq[1:,1:,:]
+        a = a[1:,1:].reshape(n_toks1, n_toks1, 1)
         reducer = TruncatedSVD(n_components=3)
-        kqat = reducer.fit_transform(kqa.reshape(n_toks1 * n_toks1, dkq))
-        colors = np.clip(0.25 + 2 * kqat.reshape(n_toks1, n_toks1, 3), 0, 1)
-        ax[layer, head].imshow(colors, interpolation='nearest')
-        ax[layer, head].set_xticks([])
-        ax[layer, head].set_yticks([])
-        ax[layer, head].set_title(f'L{layer} H{head}')
+        kqat = torch.nn.functional.normalize(torch.tensor(reducer.fit_transform(kq.reshape(n_toks1 * n_toks1, dkq))))
+        colors = np.clip(torch.abs(kqat.reshape(n_toks1, n_toks1, 3)) * (a ** 0.3), 0, 1)
+        the_ax = ax[layer - min_layer, head - min_head]
+        the_ax.imshow(colors, interpolation='nearest')
+        the_ax.set_xticks([])
+        the_ax.set_yticks([])
+        the_ax.set_title(f'L{layer} H{head}')
 plt.show()
